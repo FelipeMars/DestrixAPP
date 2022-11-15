bd468670f03f015dd6e970a80aa36e2f947137fa
Felipe Martins <felipemartoliveira@gmail.com>
2022-11-15 19:51:56 -0300

    Remove Type

--- src/app/shared/components/inputs/currency/input-currency.component.ts
+++ src/app/shared/components/inputs/currency/input-currency.component.ts
@@ -18,7 +18,6 @@ export class InputCurrencyComponent implements OnInit {
   @Input() public placeholder: string = '';
   @Input() public leftIcon: string = '';
   @Input() public controlName: string = '';
-  @Input() public type: string = 'number';
   @Input() public size: string = 'normal';
   @Input() public disabled: boolean = false;
   @Input() public floatLabel: boolean = true;
